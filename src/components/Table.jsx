220f5e8fa270c73b88627884fd802abd4c56d13c
Victor Spichenkoff Santana <victorspich13@gmail.com>
2023-07-04 18:36:24 -0300

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -102,7 +102,8 @@ export default function Table(props) {
             }
 
             return (
-                <tr key={product.id} className={style.tr}>
+                <div key={product.id}>
+                <tr className={style.tr}>
                     <td>
                         <input type="checkbox" checked={added} 
                             onClick={() => toggleAdded()}
@@ -114,6 +115,8 @@ export default function Table(props) {
                     <td>{finalPrice}</td>
                     <td>{createActions(product)}</td>
                 </tr>
+                </div>
+
             )
         })//som se receber os clientes
     }
