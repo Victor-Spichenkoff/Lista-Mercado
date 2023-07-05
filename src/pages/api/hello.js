e53e0869bcf624ff564e7d6d9d112a1c9329ce33
Victor Spichenkoff Santana <victorspich13@gmail.com>
2023-07-04 21:46:05 -0300

    Fixed key problem

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-export default async function handler(req, res) {
-  const caminho = './products.json'
-  let products
-   fs.readFile(caminho, 'utf-8', (e, conteudo ) => {
-    // const obj = JSON.parse(conteudo)
-    // products = obj
-    console.log(products)
-    res.status(200).json({ conteudo })
-})
-}
+// export default async function handler(req, res) {
+//   const caminho = './products.json'
+//   let products
+//    fs.readFile(caminho, 'utf-8', (e, conteudo ) => {
+//     // const obj = JSON.parse(conteudo)
+//     // products = obj
+//     console.log(products)
+//     res.status(200).json({ conteudo })
+// })
+// }
