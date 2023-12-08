a7478c9c5cc0d32e03021cb8e85c3bff09252c75
Victor Spichenkoff <victorspich13@gmail.com>
2023-12-07 21:16:09 -0300

    Fix favicon bug

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel='shortcut icon' type='image/x-icon' href='./favicon2.ico' />
+        <title>Lista de Compras</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
